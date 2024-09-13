45ff9ee82ce3fc59edbc8d708f143bad79096525
Fredaobob <lokgamer01@gmail.com>
2024-09-13 13:23:30 -0300

    started the development

---
+++ src/components/main-container.js
@@ -0,0 +1,12 @@
+import './styles/main.css'
+
+const MainContainer = () => {
+  return (  
+    <div className='wrapper'>
+      <img src="fred.webp" alt=""/>
+      <p>@ale.dev</p>
+    </div>
+  );
+}
+
+export default MainContainer;
