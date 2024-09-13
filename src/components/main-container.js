62aa710a52a7eca60212d4e8b895d7af8b8ce43d
Fredaobob <lokgamer01@gmail.com>
2024-09-13 13:54:41 -0300

    feat: finished the main skeleton, gotta add the mode toggler and responsiviness.

--- src/components/main-container.js
+++ src/components/main-container.js
@@ -3,8 +3,22 @@ import './styles/main.css'
 const MainContainer = () => {
   return (  
     <div className='wrapper'>
-      <img src="fred.webp" alt=""/>
+      <img id='profile-pic' src="fred.webp" alt=""/>
       <p>@ale.dev</p>
+      <button className='toggler'>TODO</button>
+      <nav className='nav-buttons'>
+        <button>TODO</button>
+        <button>TODO</button>
+        <button>TODO</button>
+        <button>TODO</button>
+      </nav>
+      <div className='socials'>
+        <img src="GitHub.svg" alt=""/>
+        <img src="Instagram.svg" alt=""/>
+        <img src="YouTube.svg" alt=""/>
+        <img src="LinkedIn.svg" alt=""/>
+      </div>
+      <p id='copy'>&copy; Alexandre Rodrigues</p>
     </div>
   );
 }
